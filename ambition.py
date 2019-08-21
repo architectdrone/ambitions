0ebb61a2c873332a4d78c640e1f7148da8b491ef
Owen Mellema <owen.mellema@hyland.com>
2019-08-21 17:02:13 -0500

    Initial

---
+++ ambition.py
@@ -0,0 +1,63 @@
+import sqlite3
+
+schema = """
+CREATE TABLE ambitions (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+CREATE TABLE virtues (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    ambition INTERGER,
+    FOREIGN KEY(ambition) REFERENCES ambitions(id)
+);
+
+CREATE TABLE finite_projects(
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    parent INTEGER,
+    infinite_project INTEGER,
+    FOREIGN KEY(parent) REFERENCES finite_projects(id),
+    FOREIGN KEY(infinite_project) REFERENCES infinite_projects(id)
+);
+
+CREATE TABLE infinite_projects(
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    parent INTEGER,
+    FOREIGN KEY(parent) REFERENCES finite_projects(id)
+);
+
+CREATE TABLE tasks(
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    finite_project INTERGER,
+    parent INTEGER,
+    FOREIGN KEY(finite_project) REFERENCES finite_projects(id),
+    FOREIGN KEY(parent) REFERENCES tasks(id),
+);
+
+CREATE TABLE positives(
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    infinite_project INTEGER,
+    FOREIGN KEY(infinite_project) REFERENCES infinite_projects(id)
+);
+
+CREATE TABLE negatives(
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    infinite_project INTEGER,
+    FOREIGN KEY(infinite_project) REFERENCES infinite_projects(id)
+);
+"""
+
+connection = sqlite3.connect("ambition.db")
+crsr = connection.cursor()
+
+createNew = True
+if createNew:
+    crsr.execute(schema)
+    connection.commit()
+
