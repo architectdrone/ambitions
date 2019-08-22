9b4103a8ff29e112657326d7e629dc76fd009765
Owen Mellema <owen.mellema@hyland.com>
2019-08-22 09:22:07 -0500

    Code for automatic creation

--- ambition.py
+++ ambition.py
@@ -1,63 +1,18 @@
 import sqlite3
+import os
 
-schema = """
-CREATE TABLE ambitions (
-    id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL
-);
+MOST_RECENT_DB_VERSION = 0
 
-CREATE TABLE virtues (
-    id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    ambition INTERGER,
-    FOREIGN KEY(ambition) REFERENCES ambitions(id)
-);
+DB_NAME = 'ambition.db'
 
-CREATE TABLE finite_projects(
-    id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    parent INTEGER,
-    infinite_project INTEGER,
-    FOREIGN KEY(parent) REFERENCES finite_projects(id),
-    FOREIGN KEY(infinite_project) REFERENCES infinite_projects(id)
-);
-
-CREATE TABLE infinite_projects(
-    id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    parent INTEGER,
-    FOREIGN KEY(parent) REFERENCES finite_projects(id)
-);
-
-CREATE TABLE tasks(
-    id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    finite_project INTERGER,
-    parent INTEGER,
-    FOREIGN KEY(finite_project) REFERENCES finite_projects(id),
-    FOREIGN KEY(parent) REFERENCES tasks(id),
-);
-
-CREATE TABLE positives(
-    id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    infinite_project INTEGER,
-    FOREIGN KEY(infinite_project) REFERENCES infinite_projects(id)
-);
-
-CREATE TABLE negatives(
-    id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    infinite_project INTEGER,
-    FOREIGN KEY(infinite_project) REFERENCES infinite_projects(id)
-);
-"""
-
-connection = sqlite3.connect("ambition.db")
-crsr = connection.cursor()
-
-createNew = True
-if createNew:
-    crsr.execute(schema)
-    connection.commit()
+# If database doesn't exist, create it
+if not os.path.exists(DB_NAME):
+    print(f"Creating Database, and updating it to version {MOST_RECENT_DB_VERSION}")
+    connection = sqlite3.connect(DB_NAME)
+    crsr = connection.cursor()
+    for i in range(MOST_RECENT_DB_VERSION+1):
+        with open(f'schemas\\{i}.sql', 'r') as f:
+            crsr.executescript(f.read())
+    print("Okay, Done.")
+    connection.close()
 
