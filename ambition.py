5589dfc81168399cfbf4df1fe7939b41f70d0571
Owen Mellema <owen.mellema@hyland.com>
2019-08-22 09:41:11 -0500

    Add auto-updating to database

--- ambition.py
+++ ambition.py
@@ -1,7 +1,7 @@
 import sqlite3
 import os
 
-MOST_RECENT_DB_VERSION = 0
+MOST_RECENT_DB_VERSION = 1
 
 DB_NAME = 'ambition.db'
 
@@ -14,5 +14,24 @@ if not os.path.exists(DB_NAME):
         with open(f'schemas\\{i}.sql', 'r') as f:
             crsr.executescript(f.read())
     print("Okay, Done.")
+    crsr.execute(f"PRAGMA user_version = {MOST_RECENT_DB_VERSION}")
+    connection.commit()
     connection.close()
 
+connection = sqlite3.connect(DB_NAME)
+crsr = connection.cursor()
+
+#Make sure the database is up to date.
+crsr.execute("PRAGMA user_version")
+current_db_version = crsr.fetchone()[0]
+print(f"You are on DB version {current_db_version}. Most recent DB version: {MOST_RECENT_DB_VERSION}")
+if current_db_version != MOST_RECENT_DB_VERSION:
+    print("Updating to most recent version...")
+    distance = MOST_RECENT_DB_VERSION - current_db_version
+    for i in range(distance):
+        current = i+1+current_db_version
+        with open(f'schemas\\{current}.sql', 'r') as f:
+            crsr.executescript(f.read())
+    print("Okay, done.")
+    crsr.execute(f"PRAGMA user_version = {MOST_RECENT_DB_VERSION}")
+    connection.commit()
